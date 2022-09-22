896f55f243ec1154bf50fa96d149b78f0a2b9fe7
Mohammad Rayyan <rayyan.dynamic@gmail.com>
2022-09-22 10:22:02 +0530

    Update PropertyFileParserMain.java

--- src/com/file/parser/PropertyFileParserMain.java
+++ src/com/file/parser/PropertyFileParserMain.java
@@ -1,11 +1,14 @@
 package com.file.parser;
 
+import java.io.IOException;
+
 public class PropertyFileParserMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
-
+		ProperFileParserService fileParser = new ProperFileParserService();
+		fileParser.getPropertyValueForKey("valuetoSearchInPropertyFile", "PropertyFileToCheck");
+			
 	}
 
 }
